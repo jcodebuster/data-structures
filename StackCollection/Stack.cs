de4ec6fd425e9755f37f679201ab55a1a50240df
Arsenii Merkulov <arsenii.vm@gmail.com>
2022-03-31 00:51:01 +0300

    added basic functionality for stack

--- StackCollection/Stack.cs
+++ StackCollection/Stack.cs
@@ -1,15 +1,79 @@
-﻿namespace StackCollection;
+﻿using System.Collections;
 
-public class Stack<T>
+namespace StackCollection;
+
+public class Stack<T> : IEnumerable<T>
 {
-    private T[] _elements;
+    private T[] _array;
 
-    public int Count { get; set; }
+    public int Count { get; private set; }
+
     private int _top;
 
-  
-    
-    
-    
+    private Stack()
+    {
+        _array = new T[4];
+    }
 
+    public Stack(int initialCapacity) : this()
+    {
+        _array = new T[initialCapacity];
+        _top = 0;
+        Count = 0;
+    }
+
+    public Stack(IEnumerable<T> collection)
+    {
+        foreach (var item in collection)
+        {
+            Push(item);
+        }
+    }
+
+    public void Push(T element)
+    {
+        if (Count == _array.Length)
+        {
+            var newArr = new T[_array.Length * 2];
+            Array.Copy(_array, newArr, _array.Length);
+            _array = newArr;
+        }
+        _array[_top++] = element;
+        Count++;
+    }
+
+    public T Peek()
+    {
+        if (Count == 0)
+        {
+            throw new InvalidOperationException("Stack is empty");
+        }
+
+        return _array[--_top];
+    }
+
+    public T Pop()
+    {
+        if (Count == 0)
+        {
+            throw new InvalidOperationException("Stack is empty");
+        }
+
+        var value = _array[--_top];
+        _array[_top] = default;
+        return value;
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        for (var i = _top; i >= 0; i--)
+        {
+            yield return _array[i];
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
 }
